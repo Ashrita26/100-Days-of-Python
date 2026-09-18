387874c9b69bbbae3728feaa4b091d74da0b762a
Davaleswarapu V N S Ashrita <ashu26062006@gmail.com>
2026-09-18 13:05:34 +0530

    Delete Day2.py

--- Day2.py
+++
@@ -1,7 +0,0 @@
-#Tip Generator
-bill=float(input("Enter the Total Bill! ="))
-tip=float(input("Enter the percentage of bill you want to give? (Like 1...2...4..5..10..) ="))
-ppl=int(input("Enter how many people to split the bill?"))
-tip_per=bill*tip/100
-each_one=(bill+tip_per)/ppl
-print("Each person must pay:",round(each_one,2))
